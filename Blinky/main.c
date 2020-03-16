f74f6bcbf8a67166194dc1aed7491ec5168baac3
ChapyClood <claudiocaponio@hispeed.ch>
2020-03-16 02:29:05 -0700

    Blinky written

--- Blinky/main.c
+++ Blinky/main.c
@@ -6,13 +6,31 @@
  */ 
 
 #include <avr/io.h>
+#include <stdint.h>
 
+void delay(){
+    volatile uint8_t i;
+    for(i=0; i<0xFF; i++){
+
+    }
+}
+
+void init_LED(){
+    DDRB |= (1<<5);
+    PORTB &= ~(1<<5); 
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    // LED_Pin: PB5
+    init_LED();
+
     while (1) 
     {
+        PORTB |= (1<<5);
+        delay();
+        PORTB &= ~(1<<5);
+        delay();
     }
 }
 
