62b6763f826512a7e8dde6ea1b12ad339ff28fe1
Edwin Koch <eddyed.k@gmail.com>
2020-03-16 09:50:56 +0100

    added empty project

---
+++ Blinky/main.c
@@ -0,0 +1,18 @@
+/*
+ * Blinky.c
+ *
+ * Created: 16-Mar-20 09:45:33
+ * Author : Edwin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
